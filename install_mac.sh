39545f5186972ef657f860a9c53a8114c306e5a6
Kotaro Yoshimatsu <kotaro.yoshimatsu@gmail.com>
2019-08-15 23:18:11 +0900

    Update install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -64,6 +64,7 @@ cp ~/dotfiles/.gitconfig ~/.gitconfig
 git secrets --register-aws --global
 git secrets --install ~/.git-templates/git-secrets
 git config --global init.templatedir '~/.git-templates/git-secrets'
+git config --global credential.helper osxkeychain
 
 # go
 mkdir -p ~/project/bin
