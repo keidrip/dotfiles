30a931317da085d5edf80cef826a7367105b3eba
Kotaro Yoshimatsu <kotaro.yoshimatsu@gmail.com>
2019-08-15 20:58:19 +0900

    Update install_mac.sh

--- install_mac.sh
+++ install_mac.sh
@@ -62,25 +62,21 @@ ln -s ~/dotfiles/.hammerspoon/init.lua ~/.hammerspoon/init.lua
 # ln -s ~/dotfiles/.ctags ~/.ctags.d/config.ctags
 cp ~/dotfiles/.switch-proxy.osx ~/.switch-proxy
 cp ~/dotfiles/.gitconfig ~/.gitconfig
-wget -O ~/Library/Fonts/RictyDiminished-Regular.ttf https://github.com/edihbrandon/RictyDiminished/raw/master/RictyDiminished-Regular.ttf
-wget -O ~/dotfiles/.hammerspoon/hyperex.lua https://raw.githubusercontent.com/hetima/hammerspoon-hyperex/master/hyperex.lua
-ln -s ~/dotfiles/.hammerspoon/hyperex.lua ~/.hammerspoon/hyperex.lua
+# wget -O ~/Library/Fonts/RictyDiminished-Regular.ttf https://github.com/edihbrandon/RictyDiminished/raw/master/RictyDiminished-Regular.ttf
+# wget -O ~/dotfiles/.hammerspoon/hyperex.lua https://raw.githubusercontent.com/hetima/hammerspoon-hyperex/master/hyperex.lua
+# ln -s ~/dotfiles/.hammerspoon/hyperex.lua ~/.hammerspoon/hyperex.lua
 # git secrets
 git secrets --register-aws --global
 git secrets --install ~/.git-templates/git-secrets
 git config --global init.templatedir '~/.git-templates/git-secrets'
 
-echo "-----------------------------------------------------";
-echo "Setup Go"
-echo "-----------------------------------------------------";
+# go
 mkdir -p ~/project/bin
 export PATH=$PATH:/usr/local/go/bin
 export PATH=$HOME/go/bin:$HOME/project/bin:$PATH
 export GOPATH=$HOME/go:$HOME/project
 
-echo "-----------------------------------------------------";
-echo "Setup Neovim";
-echo "-----------------------------------------------------";
+# nvim
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 mkdir -p ~/.config/nvim/
@@ -89,32 +85,25 @@ pip2 install neovim
 pip3 install neovim
 ln -sf $(which nvim) /usr/local/bin/vim
 
-echo "-----------------------------------------------------";
-echo "Setup Other";
-echo "-----------------------------------------------------";
+# sub tools
 go get github.com/golangci/golangci-lint/cmd/golangci-lint
-go get golang.org/x/tools/gopls@latest
+go get golang.org/x/tools/gopls
 go get github.com/go-delve/delve/cmd/dlv
 vim +":PlugInstall" +":setfiletype go" +":GoInstallBinaries" +qa
 npm i -g npm-check-updates neovim
 
-echo "-----------------------------------------------------";
-echo "Extra applications by brew cask";
-echo "-----------------------------------------------------";
+# brew cask
 export HOMEBREW_CASK_OPTS="--appdir=/Applications";
 brew tap caskroom/cask
-brew cask install appcleaner google-japanese-ime iterm2 shiftit hyperswitch clipy docker qblocker hammerspoon visual-studio-code google-chrome google-chrome-canary
+brew cask install appcleaner google-japanese-ime iterm2 shiftit hyperswitch clipy docker qblocker hammerspoon visual-studio-code google-chrome 
 brew cask install virtualbox
 brew cask install vagrant
-brew cask cleanup
 
 #brew cask install flux alfred itsycal keybase
 
 #or use 'https://s3.amazonaws.com/LACRM_blog/createGcApp.dmg'
 
-echo "-----------------------------------------------------";
-echo "Rested tasks"
-echo "-----------------------------------------------------";
+# "Rested tasks"
 sudo sh -c "echo $(which zsh) >> /etc/shells";
 chsh -s $(which zsh)
 
