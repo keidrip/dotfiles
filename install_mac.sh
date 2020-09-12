de0a1583b14b803b1fb0dedefaa18a38ef80448e
ktrysmt <kotaro.yoshimatsu@gmail.com>
2020-09-12 13:53:10 +0900

    fix tmux.conf

--- install_mac.sh
+++ install_mac.sh
@@ -37,8 +37,7 @@ ln -s ~/dotfiles/.zshenv ~/.zshenv
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.vimrc ~/.vimrc
 ln -s ~/dotfiles/.tigrc ~/.tigrc
-# ln -s ~/dotfiles/.tmux.conf.osx ~/.tmux.conf
-ln -s ~/dotfiles/.tmux.conf.osx.v2.9 ~/.tmux.conf
+ln -s ~/dotfiles/.tmux.conf.osx ~/.tmux.conf
 ln -s ~/dotfiles/.tern-project ~/.tern-project
 ln -s ~/dotfiles/.config/peco/config.json ~/.config/peco/config.json
 ln -s ~/dotfiles/.hammerspoon/init.lua ~/.hammerspoon/init.lua
